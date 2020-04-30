431dd8af987c46c6b1c91d275b591ca683423d21
zhangyaqiang <1126917181@qq.com>
2020-04-30 09:23:51 +0800

    我学保

--- src/compiler/parser/html-parser.js
+++ src/compiler/parser/html-parser.js
@@ -33,7 +33,7 @@ const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s
 // 匹配动态标签属性
 const dynamicArgAttribute = /^\s*((?:v-[\w-]+:|@|:|#)\[[^=]+\][^\s"'<>\/=]*)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/
 
-//source属性 返回用于返回模式匹配所用到的文本，不包括正则表达式直接量使用的界定符 也不包括 g m i 
+//source属性 返回用于返回模式匹配所用到的文本，不包括正则表达式直接量使用的界定符 也不包括 g m i
 const ncname = `[a-zA-Z_][\\-\\.0-9_a-zA-Z${unicodeRegExp.source}]*`
 const qnameCapture = `((?:${ncname}\\:)?${ncname})`
 // 开始标签的开头部分
@@ -44,8 +44,6 @@ const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`)
 const doctype = /^<!DOCTYPE [^>]+>/i
 // #7298: escape - to avoid being passed as HTML comment when inlined in page
 const comment = /^<!\--/
-
-// <!--[if IE]> html代码 <![endif]--> //条件注释分两部分 第一部分为普通注释,第二部分为条件注释
 const conditionalComment = /^<!\[/
 
 // Special Elements (can contain anything)
@@ -68,11 +66,8 @@ const encodedAttrWithNewLines = /&(?:lt|gt|quot|amp|#39|#10|#9);/g
 const isIgnoreNewlineTag = makeMap('pre,textarea', true)
 const shouldIgnoreFirstNewline = (tag, html) => tag && isIgnoreNewlineTag(tag) && html[0] === '\n'
 
-// 解码HTML实体
 function decodeAttr(value, shouldDecodeNewlines) {
   const re = shouldDecodeNewlines ? encodedAttrWithNewLines : encodedAttr
-  // replace() 替换 第一个参数可以是一个正则表达式 第二个参数可以是一个函数
-  // 参数 函数会对每一个匹配到的match进行处理 然后你可以将结果return 出来
   return value.replace(re, match => decodingMap[match])
 }
 
@@ -95,7 +90,6 @@ export function parseHTML(html, options) {
 
           if (commentEnd >= 0) {
             // 注释的钩子函数可以通过选项来配置只有options.shouldKeepComment为真是才会触发注释的钩子函数
-            // 默认情况下 comments 选项的值为 false ，即不保留注释，假如将其设置为 true ，则当解析器遇到注释节点时会保留该注释节点
             // 否则只截取字符串
             if (options.shouldKeepComment) {
               options.comment(html.substring(4, commentEnd), index, index + commentEnd + 3)
@@ -107,7 +101,6 @@ export function parseHTML(html, options) {
         }
 
         // http://en.wikipedia.org/wiki/Conditional_comment#Downlevel-revealed_conditional_comment
-        // 条件注释不需要触发钩子函数 只需要把它截取掉
         if (conditionalComment.test(html)) {
           const conditionalEnd = html.indexOf(']>')
 
@@ -118,7 +111,6 @@ export function parseHTML(html, options) {
         }
 
         // Doctype:
-        // 不需要触发钩子函数 只需要将匹配到这一段字符串截取掉即可，根据匹配到的字符的length属性来决定要截取多长的字符串
         const doctypeMatch = html.match(doctype)
         if (doctypeMatch) {
           advance(doctypeMatch[0].length)
@@ -129,11 +121,11 @@ export function parseHTML(html, options) {
         // 匹配结束标签
         const endTagMatch = html.match(endTag)
         if (endTagMatch) {
-          // 如果有匹配值做两件事  
+          // 如果有匹配值做两件事
           const curIndex = index
           //一截取模版
           advance(endTagMatch[0].length)
-          //二,触发钩子函数 参数为标签名 以及结束标签在原html中的开始和结束位置
+          //二,触发钩子函数
           parseEndTag(endTagMatch[1], curIndex, index)
           continue
         }
@@ -270,14 +262,13 @@ export function parseHTML(html, options) {
     }
   }
 
-  
   function handleStartTag(match) {
-    // 开始标签的匹配结果,匹配成功之后返回match对象
+    // 获取标签名
     const tagName = match.tagName
+    // 是否是自闭合标签
     const unarySlash = match.unarySlash
-     
+
     if (expectHTML) {
-      // 段落式元素
       if (lastTag === 'p' && isNonPhrasingTag(tagName)) {
         parseEndTag(lastTag)
       }
@@ -286,51 +277,26 @@ export function parseHTML(html, options) {
       }
     }
 
-
-       // 如果startTagMatch有返回值即为形式如下
-        // {
-        //   tagName:"div",
-        //   attrs:[[
-        //     ' class="box"', 'class', '=' null,null,null],
-        //   ],[' id="el"','id','=','el',null,null]]
-        // }
-
-   // unary为true 自闭合标签 为false为二元标签
     const unary = isUnaryTag(tagName) || !!unarySlash
-    
-    // 存储开始标签的attrs的长度
+
     const l = match.attrs.length
     const attrs = new Array(l)
-    // for循环的作用 格式化match中的attrs数组
     for (let i = 0; i < l; i++) {
       const args = match.attrs[i]
-      // match中的attrs数组元素格式 [
-// 如下
-//   ' v-if="isSucceed"',
-//   'v-if',
-//   '=',
-//   'isSucceed',
-//   undefined,
-//   undefined
-// ]// 第 456项之一有可能会包含属性值 如果没有属性值则默认空字符串
       const value = args[3] || args[4] || args[5] || ''
-
-      // shouldDecodeNewLines为true vue模版编译要对属性中换行符或制表符进行兼容处理
-      // shouldDecodeNewLinesForHref vue模版编译要对a标签的href属性中的换行符和制表符进行兼容处理
       const shouldDecodeNewlines = tagName === 'a' && args[1] === 'href' ?
         options.shouldDecodeNewlinesForHref :
         options.shouldDecodeNewlines
       attrs[i] = {
-        name: args[1],// 属性名
-        value: decodeAttr(value, shouldDecodeNewlines) // 属性值并对其进行html实体解码
+        name: args[1],
+        value: decodeAttr(value, shouldDecodeNewlines)
       }
       if (process.env.NODE_ENV !== 'production' && options.outputSourceRange) {
         attrs[i].start = args.start + args[0].match(/^\s*/).length
         attrs[i].end = args.end
       }
     }
-    
-    // 当开始标签不是自闭合标签时 将开始标签入栈,目的使用栈维护DOM层级
+
     if (!unary) {
       stack.push({
         tag: tagName,
@@ -339,27 +305,14 @@ export function parseHTML(html, options) {
         start: match.start,
         end: match.end
       })
-      // 把tagName赋值给 lastTag lastTag储存的标签名始终保存着栈顶的元素
       lastTag = tagName
     }
 
-     // 如果parse选项中存在start钩子函数
-     // 把标签名 格式化后的属性名, 是否是自闭合标签 开始标签在原html中的开始或者结束位置
     if (options.start) {
       options.start(tagName, attrs, unary, match.start, match.end)
     }
   }
 
-   
-  // 处理结束标签 一，当非闭合标签缺少结束如</div>时 给警告提示
-  // 二,解析完毕栈非空,处理stack栈中剩余未被处理的标签
-  // 三,解析</br>或者其他标签和</p> 保持和浏览器行为一致
-  // 如果只写结束标签浏览器 会把</br>解析为<br> 把 </p>解析为<p></p> 会忽略其他标签
- 
-  // parseEndTag根据调用参数不同 分别处理对应的情况
-  // 一,处理普通的结束标签 三个参数都传
-  // 二,只传一个lastTag参数
-  // 三,不传参数 处理stack中剩余未处理的标签
   function parseEndTag(tagName, start, end) {
     let pos, lowerCasedTagName
     if (start == null) start = index
